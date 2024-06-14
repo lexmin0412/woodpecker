b22f6ae2481c390314012c72ddc4432c7ea54b2e
lexmin0412 <zhangle_dev@outlook.com>
2024-06-14 17:25:26 +0800

    refactor: 删除无用的 svg 定义

--- app/page.tsx
+++ app/page.tsx
@@ -27,20 +27,21 @@ export default async function Home() {
 							className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
 						>
 							<span className="sr-only">Open main menu</span>
-							<svg
+							{/* <svg
 								className="h-6 w-6"
 								fill="none"
 								viewBox="0 0 24 24"
-								stroke-width="1.5"
+								strokeWidth="1.5"
 								stroke="currentColor"
 								aria-hidden="true"
 							>
 								<path
-									stroke-linecap="round"
-									stroke-linejoin="round"
+									strokeLinecap="round"
+									strokeLinejoin="round"
 									d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
 								/>
-							</svg>
+							</svg> */}
+							<span>open</span>
 						</button>
 					</div>
 					{/* <div className="hidden lg:flex lg:gap-x-12">
@@ -81,31 +82,33 @@ export default async function Home() {
 						<div className="flex items-center justify-between">
 							<a href="#" className="-m-1.5 p-1.5">
 								<span className="sr-only">Your Company</span>
-								<img
+								{/* <Image
 									className="h-8 w-auto"
+									width={32}
+									height={32}
 									src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
 									alt=""
-								/>
+								/> */}
 							</a>
 							<button
 								type="button"
 								className="-m-2.5 rounded-md p-2.5 text-gray-700"
 							>
 								<span className="sr-only">Close menu</span>
-								<svg
+								{/* <svg
 									className="h-6 w-6"
 									fill="none"
 									viewBox="0 0 24 24"
-									stroke-width="1.5"
+									strokeWidth="1.5"
 									stroke="currentColor"
 									aria-hidden="true"
 								>
 									<path
-										stroke-linecap="round"
-										stroke-linejoin="round"
+										strokeLinecap="round"
+										strokeLinejoin="round"
 										d="M6 18L18 6M6 6l12 12"
 									/>
-								</svg>
+								</svg> */}
 							</button>
 						</div>
 						<div className="mt-6 flow-root">
