e2b0372e1066a39ee4f4e05c79ecb2e5dc51caf4
lexmin0412 <zhangle_dev@outlook.com>
2024-06-16 16:48:17 +0800

    feat: 支持点击菜单回到列表页

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,30 +1,25 @@
 "use client"
 
+import Link from "next/link";
 import { useState } from "react";
 
 export default function Sidebar() {
 	const [currentMenu, setCurrentMenu] = useState("problems");
 
-	const handleClick = (name: string) => {
-		setCurrentMenu(name);
-	};
-
-	console.log("currentMenu", currentMenu);
-
 	return (
 		<div className="px-4">
 			<div className="text-gray-400 text-xs h-9 leading-9">代码质量</div>
 			<div>
-				<div
+				<Link
 					className={`text-sm h-10 leading-10 hover:text-white cursor-pointer px-2 rounded-xl ${
 						currentMenu === "problems"
 							? "text-white bg-[#1F2937]"
 							: "text-gray-400"
 					}`}
-					onClick={() => handleClick("problems")}
+					href='/dashboard'
 				>
 					规范检测
-				</div>
+				</Link>
 			</div>
 		</div>
 	);
