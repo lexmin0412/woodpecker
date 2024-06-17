d0eebccb78d71150a8915f80347502bff654e131
lexmin0412 <zhangle_dev@outlook.com>
2024-06-17 21:52:05 +0800

    feat: knip 结果可视化

--- app/qualities/lint/page.tsx
+++ app/qualities/lint/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { Accordion } from "@/components/ui/accordion";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Table, TableBody, TableCaption, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import fetch from "@toolkit-fe/request";
 import Link from "next/link";
@@ -121,45 +122,56 @@ export default function LintQualityPage({
 					className="text-sm rounded h-7 leading-7 w-16 bg-blue-600  text-gray-100 hover:scale-105 transition-all"
 					onClick={handleClick}
 				>
-				 {loading ? '检测中...' : '检测'}
+					{loading ? "检测中..." : "检测"}
 				</button>
 			</div>
 
 			<div className="flex-1 flex-col flex overflow-hidden w-full px-6 py-6">
-
-				<Tabs defaultValue="lint" value={tabValue} onValueChange={handleTabChange} className='w-full h-full flex flex-col'>
+				<Tabs
+					defaultValue="lint"
+					value={tabValue}
+					onValueChange={handleTabChange}
+					className="w-full h-full flex flex-col"
+				>
 					<TabsList className={`grid w-full grid-cols-2`}>
-						{
-							FunctionTabs.map((item)=>{
-								return (
-									<TabsTrigger key={item.value} value={item.value}>{item.label}</TabsTrigger>
-								)
-							})
-						}
+						{FunctionTabs.map((item) => {
+							return (
+								<TabsTrigger key={item.value} value={item.value}>
+									{item.label}
+								</TabsTrigger>
+							);
+						})}
 					</TabsList>
-					{
-						tabValue === 'lint' ?
+					{tabValue === "lint" ? (
 						<TabsContent value="lint">
 							<Card>
 								<CardHeader>
-									<CardTitle>{typeCountMap.error} Errors, {typeCountMap.warning} Warnings.</CardTitle>
+									<CardTitle>
+										{typeCountMap.error} Errors, {typeCountMap.warning}{" "}
+										Warnings.
+									</CardTitle>
 									<CardDescription>
-									{errText ? (
-									<div className="text-red-600">{errText}</div>
-								) : (
-									<div>最后更新时间：{lastUpdated || '未知'}</div>
-								)}
+										{errText ? (
+											<div className="text-red-600">{errText}</div>
+										) : (
+											<div>最后更新时间：{lastUpdated || "未知"}</div>
+										)}
 									</CardDescription>
 								</CardHeader>
 								<CardContent>
 									<div className="flex-1 overflow-auto">
 										{content?.map((item: IProblem) => {
 											return (
-												<div className="border border-solid border-gray-600 p-3 rounded-xl mb-2" key={`${item.type}-${item.otherProps?.file}-${item.otherProps?.line}-${item.otherProps?.col}`}>
+												<div
+													className="border border-solid border-gray-600 p-3 rounded-xl mb-2"
+													key={`${item.type}-${item.otherProps?.file}-${item.otherProps?.line}-${item.otherProps?.col}`}
+												>
 													<div className="flex items-start">
 														<div>{item.type}</div>
 														<span className="mr-1">:</span>
-														<div className="text-yellow-600">{item.otherProps?.title}</div>
+														<div className="text-yellow-600">
+															{item.otherProps?.title}
+														</div>
 													</div>
 													<div>
 														<span className="mr-1 mt-1">file:</span>
@@ -167,7 +179,7 @@ export default function LintQualityPage({
 														<Link
 															className="text-blue-600 hover:underline cursor-pointer"
 															href={`https://github.com/${userName}/${repoName}/blob/main/${item.otherProps?.file}#L${item.otherProps?.line}`}
-															target='_blank'
+															target="_blank"
 														>
 															{item.otherProps?.file}
 															{":"}
@@ -184,43 +196,122 @@ export default function LintQualityPage({
 								</CardContent>
 							</Card>
 						</TabsContent>
-						:
-						tabValue === 'unused' ?
+					) : tabValue === "unused" ? (
 						<TabsContent value="unused" className="flex-1 overflow-y-auto">
 							<Card>
 								<CardHeader>
-									<CardTitle>Unused</CardTitle>
+									<CardTitle>未使用模块</CardTitle>
 									<CardDescription>
-									{errText ? (
-									<div className="text-red-600">{errText}</div>
-								) : (
-									<div>最后更新时间：{lastUpdated || '未知'}</div>
-								)}
+										{errText ? (
+											<div className="text-red-600">{errText}</div>
+										) : (
+											<div>最后更新时间：{lastUpdated || "未知"}</div>
+										)}
 										本结果使用 knip v5.20 输出。
 									</CardDescription>
 								</CardHeader>
 								<CardContent className="space-y-2">
+									<div className="text-lg font-semibold">未使用文件</div>
+									<Table>
+										<TableCaption>
+											共 {content.files?.length || 0} 条
+										</TableCaption>
+										<TableHeader>
+											<TableRow>
+												<TableHead className="w-[100px]">Name</TableHead>
+												<TableHead>File Link</TableHead>
+											</TableRow>
+										</TableHeader>
+										<TableBody>
+											{content.files?.map((child) => (
+												<TableRow key={child}>
+													<TableCell className="font-medium">{child}</TableCell>
+													<TableCell>
+														<Link
+															target="_blank"
+															href={`https://github.com/${userName}/${repoName}/blob/main/${child}`}
+															className="hover:text-blue-600 hover:underline"
+														>
+															{`https://github.com/${userName}/${repoName}/blob/main/${child}`}
+														</Link>
+													</TableCell>
+												</TableRow>
+											))}
+										</TableBody>
+									</Table>
 									{/* 子类型 */}
+									<div className="text-lg font-semibold">未使用的其他模块</div>
 									<Accordion type="single" collapsible>
-										{JSON.stringify(content)}
-										{/* {
-											content?.map((item)=>{
-												return (
-													<AccordionItem key={item.type} value={item.type}>
-														<AccordionTrigger>{item.type}</AccordionTrigger>
-														<AccordionContent>
-															{
-																item.children?.map((child)=>{
-																	return (
-																		<div key={child.name}>{child.name}</div>
-																	)
-																})
-															}
-														</AccordionContent>
-													</AccordionItem>
-												)
-											})
-										} */}
+										{content.issues?.map((issue) => {
+											const children = Object.keys(issue)
+												.filter((key) => {
+													return issue[key].length && key !== "file";
+												})
+												.reduce((prev, cur) => {
+													console.log("issue[cur]", issue[cur]);
+													return prev.concat(
+														issue[cur].map((child) => {
+															return {
+																...child,
+																type: cur,
+															};
+														})
+													);
+												}, []);
+											return (
+												<AccordionItem key={issue.file} value={issue.file}>
+													<AccordionTrigger>{issue.file}</AccordionTrigger>
+													<AccordionContent>
+														<Table>
+															<TableCaption>
+																共 {children.length} 条
+															</TableCaption>
+															<TableHeader>
+																<TableRow>
+																	<TableHead className="w-[100px]">
+																		Name
+																	</TableHead>
+																	<TableHead>Unused Type</TableHead>
+																	<TableHead>Code Link</TableHead>
+																</TableRow>
+															</TableHeader>
+															<TableBody>
+																{children.map((child) => (
+																	<TableRow key={`${child.name}-${child.type}`}>
+																		<TableCell className="font-medium w-[200px]">
+																			{child.name}
+																		</TableCell>
+																		<TableCell className="w-[200px]">
+																			{child.type}
+																		</TableCell>
+																		<TableCell>
+																			<Link
+																				target="_blank"
+																				href={`https://github.com/${userName}/${repoName}/blob/main/${issue.file}#L${child?.line}`}
+																				className="hover:text-blue-600 hover:underline"
+																			>
+																				{`https://github.com/${userName}/${repoName}/blob/main/${issue.file}#L${child?.line}`}
+																			</Link>
+																		</TableCell>
+																	</TableRow>
+																))}
+															</TableBody>
+														</Table>
+														{/* {children?.map((child) => {
+																return (
+																	<div
+																		className="py-2 flex items-center"
+																		key={`${child.name}-${child.type}`}
+																	>
+																		<div className="w-64">{child.name}</div>
+																		{child.type}
+																	</div>
+																);
+															})} */}
+													</AccordionContent>
+												</AccordionItem>
+											);
+										})}
 									</Accordion>
 									{/* <Tabs defaultValue="lint" onValueChange={handleTabChange} className='w-full'>
 										<TabsList className={`w-full flex items-center overflow-auto flex-shrink-0 justify-start scroll`}>
@@ -251,8 +342,7 @@ export default function LintQualityPage({
 								</CardContent>
 							</Card>
 						</TabsContent>
-						: null
-					}
+					) : null}
 				</Tabs>
 			</div>
 		</div>
