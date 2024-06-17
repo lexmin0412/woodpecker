156827173ddc269672e3934378163da13a856586
lexmin0412 <zhangle_dev@outlook.com>
2024-06-17 17:21:54 +0800

    chore: 移除 google 字体引入, 规避构建报错

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import "./globals.css";
 import Main from "@/components/main";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
 	title: "Woodpecker",
@@ -18,7 +18,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body
-				className={`${inter.className} text-white flex items-center h-screen`}
+				className={`text-white flex items-center h-screen`}
 			>
 				<Main>{children}</Main>
 			</body>
