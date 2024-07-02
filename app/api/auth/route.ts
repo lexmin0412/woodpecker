5683a692b4783469284eae87fade9209950dec57
lexmin0412 <zhangle_dev@outlook.com>
2024-07-02 23:51:54 +0800

    fix: 调整重定向域名取值, 取不到的边界场景兼容

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -1,17 +1,25 @@
-import { NextResponse, NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-    console.log('request', request.headers)
+	console.log('request', request.headers)
 	const oauthClientID = process.env.GITHUB_OAUTH_APP_CLIENT_ID
-	const oauthRedirectURI = `${request.headers.get('referer')}guard/oauth`
-    console.log('oauthRedirectURI', oauthRedirectURI)
+	const origin = request.headers.get('origin')
+	if (!origin) {
+		return NextResponse.json({
+			code: -1,
+			data: null,
+			message: 'origin 为空，请求非法'
+		})
+	}
+	const oauthRedirectURI = `${origin}/guard/oauth`
+	console.log('oauthRedirectURI', oauthRedirectURI)
 	const oauthScope = process.env.GITHUB_OAUTH_APP_SCOPE
 	const authURL = `https://github.com/login/oauth/authorize?client_id=${oauthClientID}&redirect_uri=${encodeURIComponent(oauthRedirectURI as string)}&scope=${oauthScope}`
 	return NextResponse.json({
 		code: 0,
 		data: {
-            authURL,
-         },
+			authURL,
+		},
 		message: null
 	})
 }
